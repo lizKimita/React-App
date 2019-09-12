8af6c566b7e1364805fede25b7c3b77977940a7e
lizkimita <kimita.wanjiku@gmail.com>
2019-09-12 17:08:55 +0300

    react app training in progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bootstrap/dist/css/bootstrap.css";
+import Counters from "./components/counters";
 
-const element = <h1> Hello World! </h1>;
 
-ReactDOM.render(element, document.getElementById("root"));
+
+ReactDOM.render(<Counters/>, document.getElementById("root"));
